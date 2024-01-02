4abeda10de56eb74d6ff4cc1c695732567d37040
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-02 10:56:36 +0330

    add exceptions layer

---
+++ gold-certificate-issuance/src/main/java/com/gci/agent/certificate/issuance/exceptions/InvalidTrackingCodeException.java
@@ -0,0 +1,12 @@
+package com.gci.agent.certificate.issuance.exceptions;
+
+/*
+ * @author masoome.aghayari
+ * @since 12/30/23
+ */
+
+public class InvalidTrackingCodeException extends Exception {
+    public InvalidTrackingCodeException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
