d92bb4c1bffbf94f7360bb8e1920bd4a1af6f6d3
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-02 09:50:12 +0330

    add openfeign configs to

---
+++ gci-authentication/src/main/java/com/gci/authentication/config/feign/CoreFeignConfiguration.java
@@ -0,0 +1,44 @@
+package com.gci.authentication.config.feign;
+
+/*
+ * @author masoome.aghayari
+ * @since 12/31/23
+ */
+
+import feign.Logger;
+import feign.codec.Encoder;
+import feign.codec.ErrorDecoder;
+import feign.form.FormEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+@RequiredArgsConstructor
+@Configuration
+public class CoreFeignConfiguration {
+    private final ObjectFactory<HttpMessageConverters> messageConverters;
+
+    @Bean
+    @Primary
+    @Scope(SCOPE_PROTOTYPE)
+    public Encoder feignFormEncoder() {
+        return new FormEncoder(new SpringEncoder(this.messageConverters));
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
