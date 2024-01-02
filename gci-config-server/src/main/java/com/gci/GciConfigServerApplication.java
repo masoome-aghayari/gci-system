66aa97aab015ae5eb3864aeb5562b96a51acadfb
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-02 09:30:02 +0330

    add config server

---
+++ gci-config-server/src/main/java/com/gci/GciConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GciConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GciConfigServerApplication.class, args);
+	}
+
+}
