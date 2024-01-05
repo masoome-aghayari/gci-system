0b7fcc40ffa7c33575d184bb8ccf303d3f565987
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-05 21:00:49 +0330

    enhance unitTests for CertificateIssuanceServiceImpl class

--- gold-certificate-issuance/src/test/java/com/gci/agent/certificate/issuance/CertificateIssuanceTestsDataProvider.java
+++ gold-certificate-issuance/src/test/java/com/gci/agent/certificate/issuance/CertificateIssuanceTestsDataProvider.java
@@ -6,14 +6,17 @@ package com.gci.agent.certificate.issuance;
  */
 
 import com.gci.agent.certificate.issuance.mapper.CertificateRequestMapper;
+import com.gci.agent.certificate.issuance.mapper.WorkshopMapper;
 import com.gci.agent.certificate.issuance.model.dto.CertificateRequestDto;
 import com.gci.agent.certificate.issuance.model.dto.GoldDto;
 import com.gci.agent.certificate.issuance.model.dto.WorkshopAgentDto;
 import com.gci.agent.certificate.issuance.model.dto.WorkshopDto;
 import com.gci.agent.certificate.issuance.model.entity.CertificateRequest;
+import com.gci.agent.certificate.issuance.model.entity.Workshop;
 import com.gci.agent.certificate.issuance.model.enums.CertificateIssuanceStatus;
 import com.gci.agent.certificate.issuance.model.enums.GoldType;
 import com.gci.agent.certificate.issuance.repository.CertificateRequestRepository;
+import com.gci.agent.certificate.issuance.repository.WorkshopRepository;
 import com.gci.agent.certificate.issuance.service.impl.CertificateIssuanceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
 
@@ -33,7 +37,10 @@ public class CertificateIssuanceTestsDataProvider {
     protected MockMvc mockMvc;
 
     @MockBean
-    protected CertificateRequestRepository repository;
+    protected CertificateRequestRepository certificateRequestRepository;
+
+    @MockBean
+    protected WorkshopRepository workshopRepository;
 
     @Autowired
     protected CertificateIssuanceServiceImpl service;
@@ -41,41 +48,48 @@ public class CertificateIssuanceTestsDataProvider {
     @MockBean
     protected CertificateRequestMapper mapper;
 
+    @MockBean
+    protected WorkshopMapper workshopMapper;
+
+    String workshopId = getRandomId();
+    String agentId = getRandomId();
+
+    String goldId = getRandomId();
+
     protected CertificateRequestDto getCertificateRequestDto() {
         return CertificateRequestDto.builder()
-                .requester(getWorkshopAgent())
-                .trackingCode(getStringifySevenDigitsRandomNumber())
+                .trackingCode(getEightDigitsRandomString())
                 .gold(getGoldDto())
                 .status(CertificateIssuanceStatus.AWAITING_ADMIN_APPROVAL)
                 .build();
     }
 
     private GoldDto getGoldDto() {
-        return GoldDto.builder().id(getRandomId())
+        return GoldDto.builder().id(goldId)
                 .produceDate(new Date())
                 .type(GoldType.JEWELERY)
                 .weight(BigDecimal.valueOf(2.34))
-                .workShop(getWorkshopDto())
-                .code(getStringifySevenDigitsRandomNumber())
+                .workshopId(workshopId)
+                .code(getEightDigitsRandomString())
                 .build();
     }
 
-    private String getStringifySevenDigitsRandomNumber() {
-        return String.valueOf(new Random().nextInt(1_000_000, 10_000_000));
+    private String getEightDigitsRandomString() {
+        return String.valueOf(new Random().nextInt(10_000_000, 100_000_000));
     }
 
     private WorkshopDto getWorkshopDto() {
         return WorkshopDto.builder()
-                .id(getRandomId())
-                .workshopCode(getStringifySevenDigitsRandomNumber())
-                .licenseNumber(getStringifySevenDigitsRandomNumber())
-                .agent(getWorkshopAgent())
+                .id(workshopId)
+                .workshopCode(getEightDigitsRandomString())
+                .licenseNumber(getEightDigitsRandomString())
+                .agentId(agentId)
                 .build();
     }
 
     private WorkshopAgentDto getWorkshopAgent() {
         return WorkshopAgentDto.builder()
-                .id(getRandomId())
+                .id(agentId)
                 .name("John")
                 .surname("Doe")
                 .nationalCode("4310928838")
@@ -87,11 +101,17 @@ public class CertificateIssuanceTestsDataProvider {
         return UUID.randomUUID().toString();
     }
 
-    protected CertificateRequest getCertificateRequest() {
-        return mapper.dtoToEntity(getCertificateRequestDto());
+    protected CertificateRequest getCertificateRequest(CertificateRequestDto certificateRequestDto) {
+        return mapper.dtoToEntity(certificateRequestDto);
+    }
+
+    protected Optional<Workshop> getWorkshop() {
+        var workshop = workshopMapper.dtoToEntity(getWorkshopDto());
+        workshop.setDeleted(false);
+        return Optional.of(workshop);
     }
 
     protected String getTrackingCode() {
-        return getStringifySevenDigitsRandomNumber();
+        return getEightDigitsRandomString();
     }
 }
--- gold-certificate-issuance/src/test/java/com/gci/agent/certificate/issuance/service/impl/CertificateIssuanceServiceImplTest.java
+++ gold-certificate-issuance/src/test/java/com/gci/agent/certificate/issuance/service/impl/CertificateIssuanceServiceImplTest.java
@@ -2,11 +2,10 @@ package com.gci.agent.certificate.issuance.service.impl;
 
 import com.gci.agent.certificate.issuance.CertificateIssuanceTestsDataProvider;
 import com.gci.agent.certificate.issuance.exceptions.DuplicateGoldException;
-import com.gci.agent.certificate.issuance.exceptions.InvalidTrackingCodeException;
+import com.gci.agent.certificate.issuance.exceptions.NoSuchWorkshopFoundException;
 import com.gci.agent.certificate.issuance.exceptions.RequestNotFoundException;
 import com.gci.agent.certificate.issuance.model.dto.CertificateRequestDto;
 import com.gci.agent.certificate.issuance.model.entity.CertificateRequest;
-import net.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -30,23 +29,33 @@ import static org.mockito.Mockito.when;
 class CertificateIssuanceServiceImplTest extends CertificateIssuanceTestsDataProvider {
 
     @Test
-    public void given_certificateRequestDto_when_goldCode_isNot_duplicate_then_returns_expectedResult() throws DuplicateGoldException {
-        CertificateRequestDto expectedResult = getCertificateRequestDto();
-        CertificateRequest requestEntity = getCertificateRequest();
-
-        when(repository.existsByGold_Code(expectedResult.getGold().getCode())).thenReturn(false);
+    public void given_certificateRequestDto_when_goldCode_isNot_duplicate_and_workshop_found_then_returns_expectedResult()
+            throws DuplicateGoldException, NoSuchWorkshopFoundException {
+        var expectedResult = getCertificateRequestDto();
+        var requestEntity = getCertificateRequest(expectedResult);
+        when(certificateRequestRepository.existsByGold_Code(expectedResult.getGold().getCode())).thenReturn(false);
         when(mapper.dtoToEntity(expectedResult)).thenReturn(requestEntity);
-        when(repository.save(any())).thenReturn(getFilledRequestEntityById(requestEntity));
+        when(certificateRequestRepository.save(any())).thenReturn(getFilledRequestEntityById(requestEntity));
+        when(mapper.dtoToEntity(expectedResult)).thenReturn(requestEntity);
         when(mapper.entityToDto(requestEntity)).thenReturn(expectedResult);
-
-        CertificateRequestDto actualResult = service.registerCertificateIssuanceRequest(expectedResult);
+        when(workshopRepository.findByIdAndAgent_Id(expectedResult.getGold().getWorkshopId(), expectedResult.getAgentId()))
+                .thenReturn(getWorkshop());
+        var actualResult = service.registerCertificateIssuanceRequest(expectedResult);
         Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_certificateRequestDto_when_noWorkshopFound_then_throws_NoSuchWorkshopFoundException() {
+        CertificateRequestDto requestDto = getCertificateRequestDto();
+        when(workshopRepository.findByIdAndAgent_Id(requestDto.getGold().getWorkshopId(), requestDto.getAgentId()))
+                .thenReturn(Optional.empty());
+        assertThrows(NoSuchWorkshopFoundException.class, () -> service.registerCertificateIssuanceRequest(requestDto));
     }
 
     @Test
     public void given_certificateRequestDto_when_goldCode_is_duplicate_then_throws_DuplicateGoldException() {
         CertificateRequestDto requestDto = getCertificateRequestDto();
-        when(repository.existsByGold_Code(requestDto.getGold().getCode())).thenReturn(true);
+        when(certificateRequestRepository.existsByGold_Code(requestDto.getGold().getCode())).thenReturn(true);
         assertThrows(DuplicateGoldException.class,
                 () -> service.registerCertificateIssuanceRequest(requestDto));
     }
@@ -56,10 +65,9 @@ class CertificateIssuanceServiceImplTest extends CertificateIssuanceTestsDataPro
         return requestEntity;
     }
 
-
     @Test
     public void given_trackingCode_when_exists_then_returns_expectedResult()
-            throws RequestNotFoundException, InvalidTrackingCodeException {
+            throws RequestNotFoundException {
         var expectedResult = getCertificateRequestDto();
         CertificateRequestDto actualResult = service.getCertificateIssuanceRequestStatus(expectedResult.getTrackingCode());
         Assertions.assertEquals(expectedResult, actualResult);
@@ -68,14 +76,8 @@ class CertificateIssuanceServiceImplTest extends CertificateIssuanceTestsDataPro
     @Test
     public void given_trackingCode_when_notExists_then_throws_RequestNotFoundException() {
         String trackingCode = getTrackingCode();
-        when(repository.findByTrackingCode(trackingCode)).thenReturn(Optional.empty());
+        when(certificateRequestRepository.findByTrackingCode(trackingCode)).thenReturn(Optional.empty());
         assertThrows(RequestNotFoundException.class, () -> service.getCertificateIssuanceRequestStatus(trackingCode));
-    }
-
-    @Test
-    public void given_trackingCode_when_isNot_digital_then_throws_InvalidTrackingCodeException() {
-        String trackingCode = RandomString.make(10);
-        assertThrows(InvalidTrackingCodeException.class, () -> service.getCertificateIssuanceRequestStatus(trackingCode));
     }
 
 }
