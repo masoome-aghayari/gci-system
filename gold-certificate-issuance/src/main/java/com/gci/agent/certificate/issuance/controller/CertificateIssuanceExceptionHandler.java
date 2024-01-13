515825653ac1c9d729e19a2af9cb4ef7f50972a5
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-13 09:26:19 +0330

    fix CertificateIssuanceExceptionHandler bug: remove Http headers from input parameters of methods

--- gold-certificate-issuance/src/main/java/com/gci/agent/certificate/issuance/controller/CertificateIssuanceExceptionHandler.java
+++ gold-certificate-issuance/src/main/java/com/gci/agent/certificate/issuance/controller/CertificateIssuanceExceptionHandler.java
@@ -11,7 +11,6 @@ import com.gci.agent.certificate.issuance.exceptions.RequestNotFoundException;
 import com.gci.agent.certificate.issuance.model.ErrorResponse;
 import jakarta.ws.rs.ServiceUnavailableException;
 import org.springframework.beans.TypeMismatchException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -35,85 +34,70 @@ public class CertificateIssuanceExceptionHandler {
 
     @ExceptionHandler({MethodValidationException.class, TypeMismatchException.class, MissingPathVariableException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<ErrorResponse> handleMethodValidationException(Exception ex,
-                                                                            HttpHeaders headers,
-                                                                            WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.BAD_REQUEST, request.getContextPath());
+    protected ResponseEntity<ErrorResponse> handleMethodValidationException(Exception ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST, request.getContextPath());
     }
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex,
-                                                                       HttpHeaders headers,
-                                                                       WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.UNAUTHORIZED, request.getContextPath());
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED, request.getContextPath());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex,
-                                                                     HttpHeaders headers,
-                                                                     WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.FORBIDDEN, request.getContextPath());
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.FORBIDDEN, request.getContextPath());
     }
 
     @ExceptionHandler({RequestNotFoundException.class, NoResourceFoundException.class, NoSuchWorkshopFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorResponse> requestNotFoundExceptionHandler(Exception ex,
-                                                                         HttpHeaders headers,
-                                                                         WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.NOT_FOUND, request.getContextPath());
+    public ResponseEntity<ErrorResponse> requestNotFoundExceptionHandler(Exception ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, request.getContextPath());
     }
 
     @ExceptionHandler({MethodNotAllowedException.class, HttpRequestMethodNotSupportedException.class})
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    public ResponseEntity<ErrorResponse> methodNotAllowedExceptionHandler(Exception ex,
-                                                                          HttpHeaders headers,
-                                                                          WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.METHOD_NOT_ALLOWED, request.getContextPath());
+    public ResponseEntity<ErrorResponse> methodNotAllowedExceptionHandler(Exception ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.METHOD_NOT_ALLOWED, request.getContextPath());
     }
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
-                                                                             HttpHeaders headers,
                                                                              WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.NOT_ACCEPTABLE, request.getContextPath());
+        return getErrorResponse(ex.getMessage(), HttpStatus.NOT_ACCEPTABLE, request.getContextPath());
     }
 
     @ExceptionHandler(DuplicateGoldException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<ErrorResponse> duplicateGoldExceptionHandler(DuplicateGoldException ex,
-                                                                       HttpHeaders headers,
-                                                                       WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.CONFLICT, request.getContextPath());
+    public ResponseEntity<ErrorResponse> duplicateGoldExceptionHandler(DuplicateGoldException ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.CONFLICT, request.getContextPath());
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
-                                                                         HttpHeaders headers,
                                                                          WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.UNSUPPORTED_MEDIA_TYPE, request.getContextPath());
+        return getErrorResponse(ex.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE, request.getContextPath());
     }
 
 
     @ExceptionHandler({RuntimeException.class, Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErrorResponse> generalExceptionHandler(Exception ex, HttpHeaders headers, WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.INTERNAL_SERVER_ERROR, request.getContextPath());
+    public ResponseEntity<ErrorResponse> generalExceptionHandler(Exception ex, WebRequest request) {
+        return getErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, request.getContextPath());
     }
 
     @ExceptionHandler(ServiceUnavailableException.class)
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public ResponseEntity<ErrorResponse> handleServiceUnavailableException(ServiceUnavailableException ex,
-                                                                           HttpHeaders headers,
                                                                            WebRequest request) {
-        return getErrorResponse(ex.getMessage(), headers, HttpStatus.SERVICE_UNAVAILABLE, request.getContextPath());
+        return getErrorResponse(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE, request.getContextPath());
     }
 
-    private ResponseEntity<ErrorResponse> getErrorResponse(String message, HttpHeaders headers, HttpStatus status, String path) {
+    private ResponseEntity<ErrorResponse> getErrorResponse(String message, HttpStatus status, String path) {
         var errorResponse = new ErrorResponse(message, status, new Date(), path);
-        return new ResponseEntity<>(errorResponse, headers, status);
+        return new ResponseEntity<>(errorResponse, status);
     }
 }
